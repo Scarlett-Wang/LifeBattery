617d364bbc5aec8565ad9bf0e697dad0eb6ce927
zhangsht <zhsht201501102104@gmail.com>
2016-12-11 19:44:09 +0800

    get the latest plan

--- app/src/main/java/io/github/zhaomy6/lifebattery/MainActivity.java
+++ app/src/main/java/io/github/zhaomy6/lifebattery/MainActivity.java
@@ -1,14 +1,35 @@
 package io.github.zhaomy6.lifebattery;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private MyDB myDB;
+    private TextView title, DDL, progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDB = new MyDB(this);
+
+        title = (TextView)findViewById(R.id.m_planTitle);
+        DDL = (TextView)findViewById(R.id.m_planDDL);
+        progress = (TextView)findViewById(R.id.m_planProgress);
+
+        Cursor cursor = myDB.getAll();
+        String titleContent = "", DDLContent= "", progressContent = "";
+        while (cursor.moveToNext()) {
+            String tmp = cursor.getString(cursor.getColumnIndex("DDL"));
+            if (tmp.compareTo(DDLContent) > 0) {
+                titleContent = cursor.getString(cursor.getColumnIndex("title"));
+                DDLContent = tmp;
+                progressContent = cursor.getString(cursor.getColumnIndex("progress"));
+            }
+        }
+        title.setText(titleContent);
+        DDL.setText(DDLContent);
+        progress.setText(progressContent);
     }
 }
