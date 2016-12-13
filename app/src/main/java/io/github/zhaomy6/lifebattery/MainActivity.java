95b6ae37af57ff0aa4eb24396cab69fb5765e49a
赵明宇 <498973030@qq.com>
2016-12-13 18:10:58 +0800

    fix conflicts

--- app/src/main/java/io/github/zhaomy6/lifebattery/MainActivity.java
+++ app/src/main/java/io/github/zhaomy6/lifebattery/MainActivity.java
@@ -1,10 +1,7 @@
 package io.github.zhaomy6.lifebattery;
 
 import android.content.Intent;
-<<<<<<< HEAD
-=======
 import android.database.Cursor;
->>>>>>> cc84a0ebccce4fab7085d71bc48ac4cea2f3b5a0
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,12 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-<<<<<<< HEAD
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-=======
 public class MainActivity extends AppCompatActivity
     implements View.OnClickListener {
->>>>>>> cc84a0ebccce4fab7085d71bc48ac4cea2f3b5a0
     private MyDB myDB;
     private TextView title, DDL, progress;
     private Button planBotton, storeBotton, summaryButton;
@@ -26,21 +19,11 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-<<<<<<< HEAD
         findViewById(R.id.m_plan_button).setOnClickListener(this);
         findViewById(R.id.m_store_button).setOnClickListener(this);
         findViewById(R.id.m_summary_button).setOnClickListener(this);
 //        storeBotton= (Button) findViewById(R.id.m_store_button);
 //        summaryButton = (Button) findViewById(R.id.m_summary_button);
-=======
-        Button toPlans = (Button) findViewById(R.id.m_plan_button);
-        toPlans.setOnClickListener(this);
-        Button toStore = (Button) findViewById(R.id.m_store_button);
-        toStore.setOnClickListener(this);
-        Button toStatistic = (Button) findViewById(R.id.m_summary_button);
-        toStatistic.setOnClickListener(this);
->>>>>>> cc84a0ebccce4fab7085d71bc48ac4cea2f3b5a0
 //        myDB = new MyDB(this);
 //        title = (TextView)findViewById(R.id.m_plan_title);
 //        DDL = (TextView)findViewById(R.id.m_plan_ddl);
@@ -63,24 +46,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onClick(View view) {
-<<<<<<< HEAD
-        Intent intent = new Intent();
-        intent.setClass(MainActivity.this, MainActivity.class);
-        switch (view.getId()) {
-            case R.id.m_plan_button:
-                intent.setClass(MainActivity.this, PlansActivity.class);
-                break;
-            case R.id.m_store_button:
-                intent.setClass(MainActivity.this, PlansActivity.class);
-                break;
-            case R.id.m_summary_button:
-                intent.setClass(MainActivity.this, StatisticsActivity.class);
-                break;
-            default:
-                break;
-        }
-        startActivity(intent);
-=======
         Intent intent = null;
         switch (view.getId()) {
             case R.id.m_store_button:
@@ -98,6 +63,5 @@ public class MainActivity extends AppCompatActivity
             default:
                 Toast.makeText(this, "点击逻辑未处理", Toast.LENGTH_SHORT).show();
         }
->>>>>>> cc84a0ebccce4fab7085d71bc48ac4cea2f3b5a0
     }
 }
--- app/src/main/java/io/github/zhaomy6/lifebattery/StatisticsActivity.java
+++ app/src/main/java/io/github/zhaomy6/lifebattery/StatisticsActivity.java
@@ -1,14 +1,5 @@
 package io.github.zhaomy6.lifebattery;
 
-<<<<<<< HEAD
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class StatisticsActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-=======
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 public class StatisticsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
->>>>>>> cc84a0ebccce4fab7085d71bc48ac4cea2f3b5a0
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
     }
