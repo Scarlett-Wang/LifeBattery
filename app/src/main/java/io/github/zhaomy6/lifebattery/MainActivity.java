b6d5312ddf841e5937dcf5c0a29fbc67471e92f3
zhangsht <zhsht201501102104@gmail.com>
2016-12-13 17:56:09 +0800

    clear merger error

--- app/src/main/java/io/github/zhaomy6/lifebattery/MainActivity.java
+++ app/src/main/java/io/github/zhaomy6/lifebattery/MainActivity.java
@@ -1,10 +1,6 @@
 package io.github.zhaomy6.lifebattery;
 
 import android.content.Intent;
-<<<<<<< HEAD
-=======
-import android.database.Cursor;
->>>>>>> cc84a0ebccce4fab7085d71bc48ac4cea2f3b5a0
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,12 +8,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-<<<<<<< HEAD
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-=======
-public class MainActivity extends AppCompatActivity
-    implements View.OnClickListener {
->>>>>>> cc84a0ebccce4fab7085d71bc48ac4cea2f3b5a0
     private MyDB myDB;
     private TextView title, DDL, progress;
     private Button planBotton, storeBotton, summaryButton;
@@ -27,20 +18,16 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
-        findViewById(R.id.m_plan_button).setOnClickListener(this);
-        findViewById(R.id.m_store_button).setOnClickListener(this);
-        findViewById(R.id.m_summary_button).setOnClickListener(this);
-//        storeBotton= (Button) findViewById(R.id.m_store_button);
-//        summaryButton = (Button) findViewById(R.id.m_summary_button);
-=======
+//        findViewById(R.id.m_plan_button).setOnClickListener(this);
+//        findViewById(R.id.m_store_button).setOnClickListener(this);
+//        findViewById(R.id.m_summary_button).setOnClickListener(this);
+
         Button toPlans = (Button) findViewById(R.id.m_plan_button);
         toPlans.setOnClickListener(this);
         Button toStore = (Button) findViewById(R.id.m_store_button);
         toStore.setOnClickListener(this);
         Button toStatistic = (Button) findViewById(R.id.m_summary_button);
         toStatistic.setOnClickListener(this);
->>>>>>> cc84a0ebccce4fab7085d71bc48ac4cea2f3b5a0
 //        myDB = new MyDB(this);
 //        title = (TextView)findViewById(R.id.m_plan_title);
 //        DDL = (TextView)findViewById(R.id.m_plan_ddl);
@@ -63,7 +50,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onClick(View view) {
-<<<<<<< HEAD
         Intent intent = new Intent();
         intent.setClass(MainActivity.this, MainActivity.class);
         switch (view.getId()) {
@@ -71,33 +57,15 @@ public class MainActivity extends AppCompatActivity
                 intent.setClass(MainActivity.this, PlansActivity.class);
                 break;
             case R.id.m_store_button:
-                intent.setClass(MainActivity.this, PlansActivity.class);
+                intent.setClass(MainActivity.this, StoreActivity.class);
                 break;
             case R.id.m_summary_button:
                 intent.setClass(MainActivity.this, StatisticsActivity.class);
                 break;
             default:
+                Toast.makeText(this, "点击逻辑未处理", Toast.LENGTH_SHORT).show();
                 break;
         }
         startActivity(intent);
-=======
-        Intent intent = null;
-        switch (view.getId()) {
-            case R.id.m_store_button:
-                intent = new Intent(MainActivity.this, StoreActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.m_summary_button:
-                intent = new Intent(MainActivity.this, StatisticsActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.m_plan_button:
-                intent = new Intent(MainActivity.this, PlansActivity.class);
-                startActivity(intent);
-                break;
-            default:
-                Toast.makeText(this, "点击逻辑未处理", Toast.LENGTH_SHORT).show();
-        }
->>>>>>> cc84a0ebccce4fab7085d71bc48ac4cea2f3b5a0
     }
 }
